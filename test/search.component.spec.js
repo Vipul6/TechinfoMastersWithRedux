c8080fa73e0bd9c9cff9cf48cb529dd58cad890b
jainamit9928 <amitjain.incedo@gmail.com>
2017-11-19 22:53:37 +0530

    Update search.component.spec.js

--- test/search.component.spec.js
+++ test/search.component.spec.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import { mount,shallow } from 'enzyme'
-import SearchComponent from '../../src/js/components/search/search.jsx'
+import SearchComponent from '../src/js/components/search/search.jsx'
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
+configure({ adapter: new Adapter() });
 
 function setup() {
     const props = {
-      clearSearchKey:jest.fn(),
-      setSearchKey:jest.fn(),
-      techies:[],
+      actions:{
+        clearSearchKey:jest.fn(),
+        setSearchKey:jest.fn(),
+        getDataRequest:jest.fn()
+      },
+      planets:[{id:1,text:"da"}],
       isFetching:false,
-      visibleTechies:[],
+      visiblePlanets:[],
       searchKey:"test",
       match:{
           params:{
               id:3
           }
-      },
-      getDataRequest:jest.fn()
+      }
     }
   
     const enzymeWrapper = mount(<SearchComponent {...props} />)
@@ -30,21 +35,28 @@ function setup() {
     describe('SearchComponent', () => {
       it('should render self and subcomponents', () => {
         const { enzymeWrapper } = setup()
-        expect(enzymeWrapper.find('Loader').hasClass('spinner')).toBe(true)
         const todoInputProps = enzymeWrapper.find('InputSearch').props()
         expect(todoInputProps.searchKey).toBe("test")
-        const visibleTechiesProps = enzymeWrapper.find('VisibleTechieList').props()
+        const visibleTechiesProps = enzymeWrapper.find('VisiblePlanets').props()
         expect(visibleTechiesProps.queryParam).toEqual('test')
         expect(visibleTechiesProps.params).toEqual({id:3})
+        
       })
   
       it('should call handleSearchKey', () => {
         const { enzymeWrapper, props } = setup()
         const input = enzymeWrapper.find('InputSearch')
+        console.log(input.props().actions)
         input.props().setSearchKey('')
-        expect(props.setSearchKey.mock.calls.length).toBe(0)
-        input.props().setSearchKey('Use Redux')
-        expect(props.setSearchKey.mock.calls.length).toBe(1)
+        expect(props.actions.setSearchKey).toHaveBeenCalledTimes(1)
+        input.props().setSearchKey('fedu')
+        expect(props.actions.setSearchKey).toHaveBeenCalledTimes(2)
       })
+      it('it should render the expected HTML', () => {
+        const { enzymeWrapper, props } = setup()
+        expect(
+          enzymeWrapper.html()
+        ).toMatchSnapshot();
+      });
     })
   
